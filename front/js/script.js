981fee21edab165181344c53853b788da98f202b
Alisack Vannavong <alisackv@gmail.com>
2022-07-15 22:01:10 +0200

    init

---
+++ front/js/script.js
@@ -0,0 +1,17 @@
+function getProducts(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+    // 1 - recuperer canapé 
+    // 2 - creer bloc article en ex: html 
+    // 3 - créer autant de bloc qu'il n'y a d'article
+    // 4 - remplir les article avec les produits proposés 
